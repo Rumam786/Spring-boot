2a34000f6a67429e7a38d37b9b514326815c28af
Ruman Sukhera <92282309+Rumam786@users.noreply.github.com>
2022-10-06 15:41:11 +0500

    JSP view in spring boot

---
+++ JSP-view in Spring boot/src/test/java/com/myapp/myspringapp2/Myspringapp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.myspringapp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Myspringapp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
