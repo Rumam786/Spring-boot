9807c5a0ca9eb5314c31b47414970a75b2e2bae3
Ruman Sukhera <92282309+Rumam786@users.noreply.github.com>
2022-10-06 16:20:04 +0500

    JPA Spring boot create entity, create repository

---
+++ JPA Spring boot/src/main/java/com/tut/Spring/boot/project/with/spring/initializer/SpringBootProjectWithSpringInitializerApplication.java
@@ -0,0 +1,13 @@
+package com.tut.Spring.boot.project.with.spring.initializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectWithSpringInitializerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectWithSpringInitializerApplication.class, args);
+	}
+
+}
